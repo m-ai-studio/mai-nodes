69a5c6e330cba100626db78c06e3b6084eec1cdc
Josef Duda <josefduda@gmail.com>
2025-01-30 11:18:16 +0100

    feat: add reasoning node

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
 from .nodes.llm_text import MaiLLMText
+from .nodes.llm_reasoning import MaiLLMReasoning
 
-NODE_CLASS_MAPPINGS = {"MaiLLMText": MaiLLMText}
+NODE_CLASS_MAPPINGS = {"MaiLLMText": MaiLLMText, "MaiLLMReasoning": MaiLLMReasoning}
 
-NODE_DISPLAY_NAME_MAPPINGS = {"MaiLLMText": "mAI - LLM Text"}
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "MaiLLMText": "mAI - LLM Text",
+    "MaiLLMReasoning": "mAI - LLM Reasoning",
+}
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
