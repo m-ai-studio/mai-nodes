64dffe119048fb920c80821763dbc6bed91315b8
Josef Duda <josefduda@gmail.com>
2025-01-30 12:14:56 +0100

    feat: add llm vision node

--- __init__.py
+++ __init__.py
@@ -1,11 +1,17 @@
 from .nodes.llm_text import MaiLLMText
 from .nodes.llm_reasoning import MaiLLMReasoning
+from .nodes.llm_vision import MaiLLMVision
 
-NODE_CLASS_MAPPINGS = {"MaiLLMText": MaiLLMText, "MaiLLMReasoning": MaiLLMReasoning}
+NODE_CLASS_MAPPINGS = {
+    "MaiLLMText": MaiLLMText,
+    "MaiLLMReasoning": MaiLLMReasoning,
+    "MaiLLMVision": MaiLLMVision,
+}
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "MaiLLMText": "mAI - LLM Text",
     "MaiLLMReasoning": "mAI - LLM Reasoning",
+    "MaiLLMVision": "mAI - LLM Vision",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
